61e172621598dda3b15745d0a58150338133aa63
Никита Паутов <145455384+shamans2321@users.noreply.github.com>
2024-12-04 15:11:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp9
+{
+    class CnsoleApp9
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = { 1, 3, 5, 2, 4 };
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                int k = a[i];
+                int j = i - 1;
+
+                while (j >= 0 && a[j] > k)
+                {
+                    a[j + 1] = a[j];
+                    a[j] = k;
+                    j--;
+                }
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ bistray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1,3,5,2,4};
+        QuickSort(arr, 0, arr.Length - 1);
+        Console.WriteLine(string.Join(" ", arr));
+    }
+
+    static void QuickSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int pivot = arr[right], i = left - 1;
+            for (int j = left; j < right; j++)
+                if (arr[j] <= pivot) (arr[++i], arr[j]) = (arr[j], arr[i]);
+
+            (arr[i + 1], arr[right]) = (arr[right], arr[i + 1]);
+            int pivotIndex = i + 1;
+
+            QuickSort(arr, left, pivotIndex - 1);
+            QuickSort(arr, pivotIndex + 1, right);
+        }
+    }
+}
